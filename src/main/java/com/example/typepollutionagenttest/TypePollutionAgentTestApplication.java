79751de164e211884263305aac6989b62442b262
Christoph Dreis <christoph.dreis@freenet.de>
2022-10-28 14:15:28 +0200

    Initial commit

---
+++ src/main/java/com/example/typepollutionagenttest/TypePollutionAgentTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.typepollutionagenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypePollutionAgentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypePollutionAgentTestApplication.class, args);
+	}
+
+}
